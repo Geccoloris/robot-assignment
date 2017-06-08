e9d762a2b97a90b73e1fe77e9517969068b249a0
Laura <laura@rs-g.de>
2017-06-08 16:41:49 +0200

    corrected file extensions

--- scripts/polynomial_regression.py
+++ scripts/polynomial_regression.py
@@ -6,20 +6,22 @@ import numpy as np
 
 
 print "Use as follows: polynomial_regression.py <datafile> <degree>"
-
-with open(sys.argv[1], 'rb') as f:
-	reader = csv.DictReader(f)
-	x = []
-	y = []
-	for row in reader:
-#		print row
-		try:
-			x.append(float(row['x']))
-			y.append(float(row['y']))
-		except ValueError:
-			print( "Could not convert '{}' to a float...skipping.".format(entry))
-	coeffs = np.polyfit(x,y,sys.argv[2])
-	print "The coeffs are ", coeffs
-#	p = np.poly1d(coeffs)
-#	for i in x:
-#		print p(i)
+try:
+	with open(sys.argv[1], 'rb') as f:
+		reader = csv.DictReader(f)
+		x = []
+		y = []
+		for row in reader:
+	#		print row
+			try:
+				x.append(float(row['reality']))
+				y.append(float(row['measurement']))
+			except ValueError:
+				print( "Could not convert '{}' to a float...skipping.".format(entry))
+		coeffs = np.polyfit(x,y,sys.argv[2])
+		print "The coeffs are ", coeffs
+	#	p = np.poly1d(coeffs)
+	#	for i in x:
+	#		print p(i)
+except:
+	print "Something went wrong. Check for correct file format."
